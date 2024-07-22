a5044760fbc4aad5a8ef8ddf7fca9e7d00b3ef6d
unimarv <74916634+unimarv@users.noreply.github.com>
2024-07-22 23:08:12 +0300

    Update bpla.py

--- tests/bpla.py
+++ tests/bpla.py
@@ -1,7 +1,11 @@
 import requests
 from typing import *
 from classes import Bpla, Update_Bpla
+
 base_url = "http://192.168.32.142:8989"
+
+token="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjE2NTk4MjQsImlhdCI6MTcyMTY1NjIyNCwiaXNBZG1pbiI6ImZhbHNlIiwiaXNzIjoibnJ0Yl9icGxhX3NlcnZlciIsInN1YiI6IjY2ZGE2ODE2LWM2MmYtNDZlNC1hMThlLTg2ZmM1N2RiNzkxNyJ9.VlkfBhwj4Er2rIkU9zlzEhG4wmSoQxrVs_ObuGVNBqliKKp3EBIqX8qqU5-3M4kqkKBgXn-92r3c12UutvrodjBIZdCGCydNzOIKbFaHNk_N02t2Z859fiPlnGKB2T5f_9qd_pZTOhOmQn9BkD8BAXW_zwbm1g5jDk2mRCf3fTLa6OwFnZRWKGeitB65PDMQM0pU7ogcd3BeUjYRD1LyJyb6ABvfPXJZTOe6WDEpqLEwAfPYiVwxCYPsJgjksJ5FlhjauDxsiGJPYyKnL6eYexQ9NRsiQZsHy3u_hwIbRcD1eb4UpmQ0EyF0MzqPW0-uiaJYe-ok5X2oZwi3rmh9rQ"
+headers = {'Authorization': f'Bearer {token}'}
 
 def post_bpla(bpla) -> Tuple[int, Any]:
 	response = requests.post(f'{base_url}/bpla',  data=bpla.to_json())
