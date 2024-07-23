5d382afe49345cbd1546a69e8c15dd56a2910eb1
unimarv <74916634+unimarv@users.noreply.github.com>
2024-07-23 17:04:15 +0300

    Update test_bpla.py

--- tests/test_bpla.py
+++ tests/test_bpla.py
@@ -13,14 +13,14 @@ _bpla_id_ = ''
 
 status = 'status'
 @pytest.fixture
-def bpla():
+def bpla(): # функция, которая берёт ключи из класса Bpla
     return Bpla(bort_number='qwerty', encryption_key='1234', model='qaz', modem_id='qsx', type=11, user_id='dcf85b21-e71b-4256-848d-05041b896b7b')
 @pytest.fixture
-def update_bpla():
+def update_bpla(): # функция, которая берёт ключи из класса Update_Bpla
     return Update_Bpla(encryption_key='dhfgsd', modem_id='fgdfgdg')
 
 
-def test_post_bpla_real_request(bpla):
+def test_post_bpla_real_request(bpla): # регистрация БПЛА
     response = post_bpla(bpla)
     assert response == {
         'bpla': {
@@ -39,7 +39,7 @@ def test_post_bpla_real_request(bpla):
 
 
 
-def test_get_bpla(bpla):
+def test_get_bpla(bpla): # функция получения данных БПЛА пользователя
     response = get_bpla(user_id_name, bpla.user_id)
     bpla_id = response[bplas_name][0][bpla_id_name]
     assert response == {
@@ -58,7 +58,7 @@ def test_get_bpla(bpla):
 
 
 
-def test_delete_bpla_real_request(bpla):
+def test_delete_bpla_real_request(bpla): # функция удаления БПЛА
     global _bpla_id
     status_code, response = delete_bpla(bpla_id_name, _bpla_id)
     if status_code != 200:
@@ -67,7 +67,7 @@ def test_delete_bpla_real_request(bpla):
         assert response == {status: 'БПЛА удален'}
 
 
-def test_put_bpla_real_request(update_bpla, bpla):
+def test_put_bpla_real_request(update_bpla, bpla): # функция для обновления БПЛА
     global _bpla_id_
     status_code, response = put_bpla("bpla_id", _bpla_id, update_bpla)
     if status_code != 200:
