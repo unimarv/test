83204b3cdaebe99acb1dba05f256b6e97b371b0f
unimarv <74916634+unimarv@users.noreply.github.com>
2024-07-29 17:02:36 +0300

    Update user.py

--- tests/user.py
+++ tests/user.py
@@ -7,43 +7,43 @@ base_url = "http://192.168.32.142:9898"
 token = ttoken()
 headers = {'Authorization': f'Bearer {token}'}
 
-def post_user(user) -> Tuple[int, Any]: # функция регистрации пользователя
+def post_user(user, headers) -> Tuple[int, Any]:
 	response = requests.post(f'{base_url}/user', data=user.to_json(), headers=headers)
-	return response.json()
+	return response.json(), response.status_code
 
 
-def get_user(qkey, qvalue) -> Any: # функция получения данных одного пользователя
+def get_user(qkey, qvalue, headers) -> Any:
 	paylod = {qkey: qvalue}
 	response = requests.get(f"{base_url}/user", params=paylod, headers=headers)
-	return response.json()
+	return response.json(), response.status_code
 
 
-def get_all_users() -> Any: # функция, выводящая список всех пользователей
+def get_all_users(headers) -> Any:
 	response = requests.get(f'{base_url}/user', headers=headers)
-	return response.json()
+	return response.json(), response.status_code
 
 
-def put_user(qkey, qvalue, update_user) -> Any: # функция, обовляющая данные о пользователе(компания, почта, логин, ранг)
+def put_user(qkey, qvalue, update_user, headers) -> Any:
 	paylodp = {qkey: qvalue}
 	response = requests.put(base_url + '/user', data=update_user.to_json(), params=paylodp, headers=headers)
-	return response.json()
+	return response.json(), response.status_code
 
 
-def put_change_password_user(qkey, qvalue, npassword) -> Any: # функция смены пароля
+def put_change_password_user(qkey, qvalue, npassword) -> Any:
 	paylodc = {qkey: qvalue}
 	response = requests.put(f"{base_url}/user", data=npassword.to_json(), params=paylodc, headers=headers)
-	return response.json()
+	return response.json(), response.status_code
 
 
-def get_delete_user(qkey, qvalue) -> Any: # удаление пользователя
+def get_delete_user(qkey, qvalue, headers) -> Any:
 	paylodd = {qkey: qvalue}
 	response = requests.delete(f"{base_url}/user", params=paylodd, headers=headers)
-	return response.json()
+	return response.json(), response.status_code
 
-def choose_user(function) -> Any: # функция нужна если код запускается напрямую, тогда мы будем вводить только данные только той функции, которую будем выводить через print()
+def choose_user(function) -> Any:
 	if function == post_user:
-		user = User.from_dict({"company": "fkjhgdfkgh", "email":"fghfjkfg@fdgh.ru", "login": "admin",
-							   "password": "a", "rank": "dgjkdfhgkj"})
+		user = User.from_dict({"company": "fkjhgdfkgh", "email":"fghfjkfg@fdgh.ru", "login": "cats",
+							   "password": "QWEqwe!1", "rank": "dgjkdfhgkj"})
 		return user
 	elif function == put_user:
 		update_user = Update_User.from_dict(
@@ -54,5 +54,5 @@ def choose_user(function) -> Any: # функция нужна если код з
 		return npassword
 
 
-# user = get_user("login", "admin")
-# print(user)
+user = post_user(user, headers=headers)
+print(user)
