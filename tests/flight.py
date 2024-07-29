d30d430da27b91444cdf1b4048afa298aec32eb6
unimarv <74916634+unimarv@users.noreply.github.com>
2024-07-29 09:35:26 +0300

    Update flight.py

--- tests/flight.py
+++ tests/flight.py
@@ -10,23 +10,23 @@ flight = Flight.from_dict({"bpla_id": "fjkhgdkfjhg", "waypoints": [{"latitude": 
 
 token = ttoken()
 headers = {'Authorization': f'Bearer {token}'}
-def post_flight(flight) -> Tuple[int, Any]:
+def post_flight(flight) -> Tuple[int, Any]: # функция размещения полётного задания
 	response = requests.post(f'{base_url}/flight', data=flight.to_json(), headers=headers)
 	return response.status_code, response.json()
 
-def delete_flight(qkey, qvalue) -> Any:
+def delete_flight(qkey, qvalue) -> Any: # удаление полётного задания
 	payloadd = {qkey: qvalue}
 	response = requests.delete(f"{base_url}/flight", params=payloadd, headers=headers)
 	return response.json()
 
 
-def put_flight(qkey, qvalue) -> Any:
+def put_flight(qkey, qvalue) -> Any: # выполнение полётного задания 
 	payloadp = {qkey: qvalue}
 	response = requests.put(base_url + '/flight', params=payloadp, headers=headers)
 	return response.json()
 
 
-def get_flight(qkey, qvalue) -> Any:
+def get_flight(qkey, qvalue) -> Any: # получение полётного задания 
 	paylaod = {qkey: qvalue}
 	response = requests.get(f"{base_url}/flight", params=paylaod, headers=headers)
 	return response.json()
