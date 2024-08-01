e8b22b6e6a7d7b8b30e4f05d1edbb6f3c81333ca
unimarv <74916634+unimarv@users.noreply.github.com>
2024-08-01 11:36:51 +0300

    Update tok.py

--- tests/tok.py
+++ tests/tok.py
@@ -1,11 +1,12 @@
 # код написан для того чтобы брать токен брался автоматически, без копирования каждый раз. Нужен будет только менять в response - "cats"(как логин) и "а"(как пароль)
+import login
 from classes import Login
 
 def ttoken():
-    responsea = login.post_login(Login('admin', 'a'))
-    response = login.post_login(Login('dogs', 'a'))
+    responsea = login.post_login(Login('admin', 'admin@mail.ru', 'a'))
+    response = login.post_login(Login('dogs', 'dogs@mail.ru', 'aSdqqw!0'))
     token = response[1]['token']
     tokena = responsea[1]['token']
     return tokena, token
-print(ttoken())
+
 
