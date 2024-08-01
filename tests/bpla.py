643293dddb7ac8bc5c285b55bc58616bcb77e03e
unimarv <74916634+unimarv@users.noreply.github.com>
2024-08-01 11:34:50 +0300

    Update bpla.py

--- tests/bpla.py
+++ tests/bpla.py
@@ -5,32 +5,33 @@ from tok import *
 
 base_url = "http://192.168.32.142:9898"
 
-token = ttoken()
+token = ttoken()[1]
 headers = {'Authorization': f'Bearer {token}'}
 
-def post_bpla(bpla) -> Tuple[int, Any]: # регистрация бпла
+def post_bpla(bpla, headers) -> Tuple[int, Any]:
 	response = requests.post(f'{base_url}/bpla',  data=bpla.to_json(), headers=headers)
-	return response.json()
+	return response.status_code, response.json()
 
-def delete_bpla(qkey, qvalue) -> Any: # удаление бпла
+def delete_bpla(qkey, qvalue, headers) -> Tuple[int, Any]:
 	payloadd = {qkey: qvalue}
 	response = requests.delete(f"{base_url}/bpla", params=payloadd, headers=headers)
 	return response.status_code, response.json()
 
-def put_bpla(qkey, qvalue, update_bpla) -> Any: # функци обновления бпла по bpla_id
+def put_bpla(qkey, qvalue, update_bpla, headers) -> Tuple[int, Any]:
 	payloadp = {qkey: qvalue}
 	response = requests.put(base_url + '/bpla', data=update_bpla.to_json(), params=payloadp, headers=headers)
 	return response.status_code, response.json()
 
-def get_bpla(qkey, qvalue) -> Any: # получаем данные про бпла
+def get_bpla(qkey, qvalue, headers) -> Tuple[int, Any]:
 	paylaod = {qkey: qvalue}
 	response = requests.get(f"{base_url}/bpla", params=paylaod, headers=headers)
-	return  response.json()
+	return response.status_code, response.json()
 
-def choose_bpla(function) -> Any: # функция нужна если код запускается напрямую, тогда мы будем вводить только данные только той функции, которую будем выводить через print()
+def choose_bpla(function) -> Tuple[int, Any]:
 	if function == post_bpla:
 		bpla = Bpla.from_dict({"bort_number":'ldfjhg', "encryption_key":'fdgh', "model":'gfkdfghdflghru', "modem_id":'dfkghdfg', "type": 23, "user_id":'dcf85b21-e71b-4256-848d-05041b896b7b'})
 		return bpla
+
 	elif function == put_bpla:
 		update_bpla = Update_Bpla.from_dict({"encryption_key": "dghdfkg", "modem_id": "flhgkjdfhg"})
 		return update_bpla
