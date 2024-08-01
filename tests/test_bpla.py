40e90f2996ec479cdab6df93ba545fb4af434d6d
unimarv <74916634+unimarv@users.noreply.github.com>
2024-08-01 10:49:55 +0300

    Update test_bpla.py

--- tests/test_bpla.py
+++ tests/test_bpla.py
@@ -1,7 +1,7 @@
 import pytest
 from classes import Bpla, Update_Bpla
 from bpla import post_bpla, put_bpla, delete_bpla, get_bpla
-
+from tok import *
 
 bpla_id_name = 'bpla_id'
 bpla_name = 'bpla'
@@ -9,19 +9,28 @@ bplas_name = 'bplas'
 user_id_name = 'user_id'
 
 _bpla_id = ''
-_bpla_id_ = ''
-
+bpla_id = ''
+unknown_user_id_input = "02c124d8-0296-4417-b076-f423aa3d9d68"
+unknown_bpla_id = "d464d4b1-dfa9-43eb-9e57-2800e24119fb"
 status = 'status'
+
+non_token_admin = ttoken()[1]
 @pytest.fixture
 def bpla(): # функция, которая берёт ключи из класса Bpla
-    return Bpla(bort_number='qwerty', encryption_key='1234', model='qaz', modem_id='qsx', type=11, user_id='dcf85b21-e71b-4256-848d-05041b896b7b')
+    return Bpla(bort_number='qwerty', encryption_key='1234', model='qaz', modem_id='qsx', type=11, user_id='02c124d8-0296-4417-b076-f423aa3d9d60')
 @pytest.fixture
 def update_bpla(): # функция, которая берёт ключи из класса Update_Bpla
     return Update_Bpla(encryption_key='dhfgsd', modem_id='fgdfgdg')
 
+def test_post_bpla_unauthorized(bpla):
+    status_code, response = post_bpla(bpla, headers=None)
+    assert status_code == 401
+    assert response == {status: "Требуется авторизация"}
 
 def test_post_bpla_real_request(bpla): # регистрация БПЛА
-    response = post_bpla(bpla)
+    headers = {'Authorization': f'Bearer {non_token_admin}'}
+    status_code, response = post_bpla(bpla, headers=headers)
+    print(f"Response: {response},Status Code: {status_code}")
     assert response == {
         'bpla': {
             'bort_number': bpla.bort_number,
@@ -33,15 +42,27 @@ def test_post_bpla_real_request(bpla): # регистрация БПЛА
         },
         status: 'Новый БПЛА зарегестрирован'
     }
-    global _bpla_id_
-    _bpla_id_ = response[bpla_name][bpla_id_name]
+    global _bpla_id
+    _bpla_id = response[bpla_name][bpla_id_name]
 
+def test_get_bpla_unauthorized(bpla):
+    status_code, response = get_bpla(user_id_name, bpla.user_id, headers=None)
+    assert status_code == 401
+    assert response == {status:"Требуется авторизация"}
 
-
+def test_get_bpla_not_found(bpla):
+    headers = {'Authorization': f'Bearer {non_token_admin}'}
+    status_code, response = get_bpla(user_id_name, unknown_user_id_input, headers=headers)
+    print(f"Response: {response},Status Code: {status_code}")
+    assert status_code == 404
+    assert response == {status: "БПЛА не найден"}
 
 def test_get_bpla(bpla): # функция получения данных БПЛА пользователя
-    response = get_bpla(user_id_name, bpla.user_id)
+    global bpla_id
+    headers = {'Authorization': f'Bearer {non_token_admin}'}
+    status_code, response = get_bpla(user_id_name, bpla.user_id, headers=headers)
     bpla_id = response[bplas_name][0][bpla_id_name]
+    print(f"Response: {response},Status Code: {status_code}")
     assert response == {
         'bplas': [{
             'bort_number': bpla.bort_number,
@@ -50,38 +71,52 @@ def test_get_bpla(bpla): # функция получения данных БПЛ
             'model': bpla.model,
             'modem_id': bpla.modem_id,
             'type': bpla.type,
-            'missions': response[bplas_name][0]['missions']
+            #'missions': response['bplas'][0]['missions']
         },]
     }
+
+def test_put_bpla_unauthorized(update_bpla, bpla):
     global _bpla_id
-    _bpla_id = response[bplas_name][0][bpla_id_name]
+    status_code, response = put_bpla("bpla_id", _bpla_id, update_bpla, headers=None)
+    assert status_code == 401
+    assert response == {status:"Требуется авторизация"}
 
-
-
-def test_delete_bpla_real_request(bpla): # функция удаления БПЛА
+def test_put_bpla_not_found(update_bpla):
     global _bpla_id
-    status_code, response = delete_bpla(bpla_id_name, _bpla_id)
-    if status_code != 200:
-        assert response == {status: 'Некорректный запрос'}
-    else:
-        assert response == {status: 'БПЛА удален'}
+    headers = {'Authorization': f'Bearer {non_token_admin}'}
+    status_code, response = put_bpla(bpla_id_name, unknown_bpla_id, update_bpla, headers=headers)
+    print(f"Response: {response},Status Code: {status_code}")
+    assert status_code == 400
+    assert response == {status: "Некорректный запрос"}
 
 
 def test_put_bpla_real_request(update_bpla, bpla): # функция для обновления БПЛА
-    global _bpla_id_
-    status_code, response = put_bpla("bpla_id", _bpla_id, update_bpla)
-    if status_code != 200:
-        assert response == {status: 'Некорректный запрос'}
-    else:
-        assert response == {
-            'bpla': {
-                'bort_number': bpla.bort_number,
-                'bpla_id': _bpla_id_,
-                'encryption_key': update_bpla.encryption_key,
-                'model': bpla.model,
-                'modem_id': update_bpla.modem_id,
-                'type': bpla.type
-
+    global _bpla_id
+    headers = {'Authorization': f'Bearer {non_token_admin}'}
+    status_code, response = put_bpla(bpla_id_name, _bpla_id, update_bpla, headers=headers)
+    print(f"Response: {response},Status Code: {status_code}")
+    assert status_code == 200
+    assert response == {
+        'bpla': {
+            'bort_number': bpla.bort_number,
+            'bpla_id': _bpla_id,
+            'encryption_key': update_bpla.encryption_key,
+            'model': bpla.model,
+            'modem_id': update_bpla.modem_id,
+            'type': bpla.type
             },
-            status: 'БПЛА обновлен'
+        status: 'БПЛА обновлен'
         }
+
+def test_delete_bpla_unauthorized(bpla):
+    status_code, response = delete_bpla(bpla_id_name, bpla_id, headers=None)
+    assert status_code == 401
+    assert response == {status: "Требуется авторизация"}
+
+def test_delete_bpla_real_request(bpla):  # функция удаления БПЛА
+    global bpla_id
+    headers = {'Authorization': f'Bearer {non_token_admin}'}
+    status_code, response = delete_bpla(bpla_id_name, bpla_id, headers=headers)
+    print(f"Response: {response},Status Code: {status_code}")
+    assert status_code == 200
+    assert response == {status: 'БПЛА удален'}
